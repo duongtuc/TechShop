bbb945abb6479e52273afd25bbd8342fbf9e3318
DuongTuc <duongtuc.dev@gmail.com>
2020-02-12 22:06:06 +0700

    commit model and ignore file

---
+++ TechShop/TechShop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,31 @@
+﻿namespace TechShop.Model.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Defines the <see cref="VisitorStatistic" />
+    /// </summary>
+    [Table("VisitorStatistics")]
+    public class VisitorStatistic
+    {
+        /// <summary>
+        /// Gets or sets the ID
+        /// </summary>
+        [Key]
+        public Guid ID { set; get; }
+
+        /// <summary>
+        /// Gets or sets the VisitedDate
+        /// </summary>
+        [Required]
+        public DateTime VisitedDate { set; get; }
+
+        /// <summary>
+        /// Gets or sets the IPAddress
+        /// </summary>
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
