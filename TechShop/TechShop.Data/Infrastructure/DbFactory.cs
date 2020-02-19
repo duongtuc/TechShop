a3406022807916f1c2322d457a83e2606730a45a
DuongTuc <duongtuc.dev@gmail.com>
2020-02-19 10:16:17 +0700

    commit DAL

---
+++ TechShop/TechShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        TechShopDbContext TechShopDbContext;
+        public TechShopDbContext Init()
+        {
+            return TechShopDbContext ?? (TechShopDbContext = new TechShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (TechShopDbContext != null)
+            {
+                TechShopDbContext.Dispose();
+            }
+        }
+    }
+}
